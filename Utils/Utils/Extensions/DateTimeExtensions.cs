03191267cf642ca936f462cb636350021020facb
ErRoen <errbase-gh@yahoo.com>
2014-05-28 13:54:05 -0500

    Added some DateTime extension methods

---
+++ Utils/Utils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Extensions
+{
+	public static class DateTimeExtensions
+	{
+		public static DateTime SpecifyDateAsUtc(this DateTime dateTime)
+		{
+			return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+		}
+
+
+		public static DateTime GetMaxBetween(DateTime first, DateTime second)
+		{
+			return first > second
+				? first
+				: second;
+		}
+	}
+}
