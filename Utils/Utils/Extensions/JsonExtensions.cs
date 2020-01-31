8b321b3f9474e0c65bc883b6c08e898637cad765
Erick Roen <erick.roen@avanade.com>
2020-01-31 16:54:12 -0600

    Adding Some Extensions to test webhooks

---
+++ Utils/Utils/Extensions/JsonExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Utils.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string SerializeToJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public static byte[] SerializeToJsonByteArray(this object value)
+        {
+            var serializeAsString = value.SerializeToJson();
+            return Encoding.UTF8.GetBytes(serializeAsString);
+        }
+
+        public static TEntity DeserializeFromJson<TEntity>(this string value)
+        {
+            return JsonConvert.DeserializeObject<TEntity>(value);
+        }
+
+        public static XmlDocument DeserializeXmlNodeFromJson(this string jsonContent, string deserializeRootElementName)
+        {
+            return JsonConvert.DeserializeXmlNode(jsonContent, deserializeRootElementName);
+        }
+
+        /// <summary>
+        ///		Check return value whether xml of json
+        /// </summary>
+        /// <param name="input">Return string to check.</param>
+        /// <returns>True if Json format, otherwise False</returns>
+        public static bool IsJson(this string input)
+        {
+            var strInput = input.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}"))  //For object
+                || (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Utils/Utils/Extensions/XmlExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Utils.Extensions
+{
+    /// <summary>A helper class for working with XML strings</summary>
+    public static class XmlExtensions
+    {
+        /// <summary>Deserialize an object from an XML string.</summary>
+        /// <typeparam name="T">The type of object to deserialize into.</typeparam>
+        /// <param name="xmlMessage">A string containing only an xml representation of the object to deserialize.</param>
+        /// <returns>An object representative of the xml passed in.</returns>
+        public static T DeserializeFromXml<T>(this string xmlMessage)
+        {
+            using (var sr = new StringReader(xmlMessage))
+            {
+                var xs = new XmlSerializer(typeof(T));
+                return (T)xs.Deserialize(sr);
+            }
+        }
+
+        /// <summary>
+        ///     Serializes and object to an XML string.
+        /// </summary>
+        /// <typeparam name="T">The type of object to serialize.</typeparam>
+        /// <param name="obj">The object to serialize.</param>
+        /// <param name="xn">The namespaces to use when writing the XML.</param>
+        /// <returns>An xml string representation of a serializable object.</returns>
+        public static string SerializeToXml<T>(this T obj, XmlSerializerNamespaces xn = null)
+        {
+            var xs = new XmlSerializer(typeof(T));
+
+            using (var sw = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(sw))
+                {
+                    xs.Serialize(writer, obj, xn);
+                    return sw.ToString();
+                }
+            }
+        }
+
+        public static bool IsXml(this string input)
+        {
+            try
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(input);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
